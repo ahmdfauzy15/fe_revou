3ac03c8d75a2559d28c1b053107d396878618ebb
ahmdfauzy15 <ahfauzy15@gmail.com>
2023-11-17 20:04:54 +0700

    done navbar responsif

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,30 @@
 // //...........Nav Drop Down..............//
+const menuBtn = document.querySelector(".menu-icon");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+
+menuBtn.addEventListener("click", () => {
+items.classList.toggle("active");
+menuBtn.classList.toggle("hide");
+searchBtn.classList.toggle("hide");
+cancelBtn.classList.toggle("show");
+});
+
+cancelBtn.addEventListener("click", () => {
+items.classList.remove("active");
+menuBtn.classList.remove("hide");
+searchBtn.classList.remove("hide");
+cancelBtn.classList.remove("show");
+form.classList.remove("active");
+});
+
+searchBtn.addEventListener("click", () => {
+form.classList.add("active");
+searchBtn.classList.add("hide");
+cancelBtn.classList.add("show");
+});
 
 // document.querySelector('.Open').addEventListener('click',()=>{
 //     document.querySelector('.nav-link').style.display='flex';
